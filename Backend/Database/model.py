a2edbd3a2fff686ee015755365591fb9b8dff72e
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-04-01 15:33:17 +0530

    mongodb connection is done

--- Backend/Database/model.py
+++ Backend/Database/model.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel,Field
 from datetime import datetime
 from typing import Optional
 
-class JounralEntry(BaseModel):
+class JournalEntry(BaseModel):
     content:str=Field(...,min_length=15,description="The text body of the journal")
     mood_score: float=Field(default=5.0, ge=1.0, le=10.0)
     created_at: datetime = Field(default_factory=datetime.utcnow)
